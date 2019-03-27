f33b3ac7f7b9b60ee9a8b8d37cfaac515616c6da
liujin <wuxiongc@163.com>
2019-03-27 12:39:54 +0800

    finished

--- app/src/main/java/tech/threekilogram/colorresourceslib/MainActivity.java
+++ app/src/main/java/tech/threekilogram/colorresourceslib/MainActivity.java
@@ -1,14 +1,68 @@
 package tech.threekilogram.colorresourceslib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import tech.threekilogram.colors.Colors;
 
 public class MainActivity extends AppCompatActivity {
+
+      private RecyclerView mRecycler;
 
       @Override
       protected void onCreate ( Bundle savedInstanceState ) {
 
             super.onCreate( savedInstanceState );
             setContentView( R.layout.activity_main );
+            initView();
+      }
+
+      private void initView ( ) {
+
+            mRecycler = findViewById( R.id.recycler );
+            mRecycler.setLayoutManager( new GridLayoutManager( this, 2 ) );
+            mRecycler.setAdapter( new Adapter() );
+      }
+
+      private class Holder extends RecyclerView.ViewHolder {
+
+            public Holder ( @NonNull View itemView ) {
+
+                  super( itemView );
+            }
+
+            private void bind ( int position ) {
+
+                  itemView.setBackgroundColor( Colors.ALL[ position ] );
+            }
+      }
+
+      private class Adapter extends RecyclerView.Adapter<Holder> {
+
+            @NonNull
+            @Override
+            public Holder onCreateViewHolder ( @NonNull ViewGroup viewGroup, int i ) {
+
+                  View view = new View( MainActivity.this );
+                  view.setLayoutParams( new LayoutParams( LayoutParams.MATCH_PARENT, 200 ) );
+                  return new Holder( view );
+            }
+
+            @Override
+            public void onBindViewHolder ( @NonNull Holder holder, int i ) {
+
+                  holder.bind( i );
+            }
+
+            @Override
+            public int getItemCount ( ) {
+
+                  return Colors.ALL.length;
+            }
       }
 }
---
+++ colors/src/main/java/tech/threekilogram/colors/Colors.java
@@ -0,0 +1,287 @@
+package tech.threekilogram.colors;
+
+/**
+ * @author Liujin 2019/3/27:11:52:58
+ */
+public class Colors {
+
+      public static final int white                = 0xffffffff;
+      public static final int ivory                = 0xfffffff0;
+      public static final int lightYellow          = 0xffffffe0;
+      public static final int yellow               = 0xffffff00;
+      public static final int snow                 = 0xfffffafa;
+      public static final int floralWhite          = 0xfffffaf0;
+      public static final int lemonChiffon         = 0xfffffacd;
+      public static final int cornSilk             = 0xfffff8dc;
+      public static final int seaShell             = 0xfffff5ee;
+      public static final int lavenderBlush        = 0xfffff0f5;
+      public static final int papayaWhip           = 0xffffefd5;
+      public static final int blanchedAlmond       = 0xffffebcd;
+      public static final int mistyRose            = 0xffffe4e1;
+      public static final int bisque               = 0xffffe4c4;
+      public static final int moccasin             = 0xffffe4b5;
+      public static final int navajoWhite          = 0xffffdead;
+      public static final int peachPuff            = 0xffffdab9;
+      public static final int gold                 = 0xffffd700;
+      public static final int pink                 = 0xffffc0cb;
+      public static final int lightPink            = 0xffffb6c1;
+      public static final int orange               = 0xffffa500;
+      public static final int lightSalmon          = 0xffffa07a;
+      public static final int darkOrange           = 0xffff8c00;
+      public static final int coral                = 0xffff7f50;
+      public static final int hotPink              = 0xffff69b4;
+      public static final int tomato               = 0xffff6347;
+      public static final int orangeRed            = 0xffff4500;
+      public static final int deepPink             = 0xffff1493;
+      public static final int fuchsia              = 0xffff00ff;
+      public static final int magenta              = 0xffff00ff;
+      public static final int red                  = 0xffff0000;
+      public static final int oldLace              = 0xfffdf5e6;
+      public static final int lightGoldenrodYellow = 0xfffafad2;
+      public static final int linen                = 0xfffaf0e6;
+      public static final int antiqueWhite         = 0xfffaebd7;
+      public static final int salmon               = 0xfffa8072;
+      public static final int ghostWhite           = 0xfff8f8ff;
+      public static final int mintCream            = 0xfff5fffa;
+      public static final int whiteSmoke           = 0xfff5f5f5;
+      public static final int beige                = 0xfff5f5dc;
+      public static final int wheat                = 0xfff5deb3;
+      public static final int sandyBrown           = 0xfff4a460;
+      public static final int azure                = 0xfff0ffff;
+      public static final int honeydew             = 0xfff0fff0;
+      public static final int aliceBlue            = 0xfff0f8ff;
+      public static final int khaki                = 0xfff0e68c;
+      public static final int lightCoral           = 0xfff08080;
+      public static final int paleGoldenrod        = 0xffeee8aa;
+      public static final int violet               = 0xffee82ee;
+      public static final int darkSalmon           = 0xffe9967a;
+      public static final int lavender             = 0xffe6e6fa;
+      public static final int lightCyan            = 0xffe0ffff;
+      public static final int burlyWood            = 0xffdeb887;
+      public static final int plum                 = 0xffdda0dd;
+      public static final int gainsboro            = 0xffdcdcdc;
+      public static final int crimson              = 0xffdc143c;
+      public static final int paleVioletRed        = 0xffdb7093;
+      public static final int goldenrod            = 0xffdaa520;
+      public static final int orchid               = 0xffda70d6;
+      public static final int thistle              = 0xffd8bfd8;
+      public static final int lightGray            = 0xffd3d3d3;
+      public static final int tan                  = 0xffd2b48c;
+      public static final int chocolate            = 0xffd2691e;
+      public static final int peru                 = 0xffcd853f;
+      public static final int indianRed            = 0xffcd5c5c;
+      public static final int mediumVioletRed      = 0xffc71585;
+      public static final int silver               = 0xffc0c0c0;
+      public static final int darkKhaki            = 0xffbdb76b;
+      public static final int rosyBrown            = 0xffbc8f8f;
+      public static final int mediumOrchid         = 0xffba55d3;
+      public static final int darkGoldenrod        = 0xffb8860b;
+      public static final int firebrick            = 0xffb22222;
+      public static final int powderBlue           = 0xffb0e0e6;
+      public static final int lightSteelBlue       = 0xffb0c4de;
+      public static final int paleTurquoise        = 0xffafeeee;
+      public static final int greenYellow          = 0xffadff2f;
+      public static final int lightBlue            = 0xffadd8e6;
+      public static final int darkGray             = 0xffa9a9a9;
+      public static final int brown                = 0xffa52a2a;
+      public static final int sienna               = 0xffa0522d;
+      public static final int darkOrchid           = 0xff9932cc;
+      public static final int paleGreen            = 0xff98fb98;
+      public static final int darkViolet           = 0xff9400d3;
+      public static final int mediumPurple         = 0xff9370db;
+      public static final int lightGreen           = 0xff90ee90;
+      public static final int darkSeaGreen         = 0xff8fbc8f;
+      public static final int saddleBrown          = 0xff8b4513;
+      public static final int darkMagenta          = 0xff8b008b;
+      public static final int darkRed              = 0xff8b0000;
+      public static final int blueViolet           = 0xff8a2be2;
+      public static final int lightSkyBlue         = 0xff87cefa;
+      public static final int skyBlue              = 0xff87ceeb;
+      public static final int grey                 = 0xff808080;
+      public static final int olive                = 0xff808000;
+      public static final int purple               = 0xff800080;
+      public static final int maroon               = 0xff800000;
+      public static final int aquamarine           = 0xff7fffd4;
+      public static final int chartreuse           = 0xff7fff00;
+      public static final int lawnGreen            = 0xff7cfc00;
+      public static final int mediumSlateBlue      = 0xff7b68ee;
+      public static final int lightSlateGray       = 0xff778899;
+      public static final int slateGray            = 0xff708090;
+      public static final int oliveDrab            = 0xff6b8e23;
+      public static final int slateBlue            = 0xff6a5acd;
+      public static final int dimGray              = 0xff696969;
+      public static final int mediumAquamarine     = 0xff66cdaa;
+      public static final int cornFlowerBlue       = 0xff6495ed;
+      public static final int cadetBlue            = 0xff5f9ea0;
+      public static final int darkOliveGreen       = 0xff556b2f;
+      public static final int indigo               = 0xff4b0082;
+      public static final int mediumTurquoise      = 0xff48d1cc;
+      public static final int darkSlateBlue        = 0xff483d8b;
+      public static final int steelBlue            = 0xff4682b4;
+      public static final int royalBlue            = 0xff4169e1;
+      public static final int turquoise            = 0xff40e0d0;
+      public static final int mediumSeaGreen       = 0xff3cb371;
+      public static final int limeGreen            = 0xff32cd32;
+      public static final int darkSlateGray        = 0xff2f4f4f;
+      public static final int seaGreen             = 0xff2e8b57;
+      public static final int forestGreen          = 0xff228b22;
+      public static final int lightSeaGreen        = 0xff20b2aa;
+      public static final int dodgerBlue           = 0xff1e90ff;
+      public static final int midNightBlue         = 0xff191970;
+      public static final int aqua                 = 0xff00ffff;
+      public static final int cyan                 = 0xff00ffff;
+      public static final int springGreen          = 0xff00ff7f;
+      public static final int lime                 = 0xff00ff00;
+      public static final int mediumSpringGreen    = 0xff00fa9a;
+      public static final int darkTurquoise        = 0xff00ced1;
+      public static final int deepSkyBlue          = 0xff00bfff;
+      public static final int darkCyan             = 0xff008b8b;
+      public static final int teal                 = 0xff008080;
+      public static final int green                = 0xff008000;
+      public static final int darkGreen            = 0xff006400;
+      public static final int blue                 = 0xff0000ff;
+      public static final int mediumBlue           = 0xff0000cd;
+      public static final int darkBlue             = 0xff00008b;
+      public static final int navy                 = 0xff000080;
+
+      public static final int[] ALL = {
+          white,
+          ivory,
+          lightYellow,
+          yellow,
+          snow,
+          floralWhite,
+          lemonChiffon,
+          cornSilk,
+          seaShell,
+          lavenderBlush,
+          papayaWhip,
+          blanchedAlmond,
+          mistyRose,
+          bisque,
+          moccasin,
+          navajoWhite,
+          peachPuff,
+          gold,
+          pink,
+          lightPink,
+          orange,
+          lightSalmon,
+          darkOrange,
+          coral,
+          hotPink,
+          tomato,
+          orangeRed,
+          deepPink,
+          fuchsia,
+          magenta,
+          red,
+          oldLace,
+          lightGoldenrodYellow,
+          linen,
+          antiqueWhite,
+          salmon,
+          ghostWhite,
+          mintCream,
+          whiteSmoke,
+          beige,
+          wheat,
+          sandyBrown,
+          azure,
+          honeydew,
+          aliceBlue,
+          khaki,
+          lightCoral,
+          paleGoldenrod,
+          violet,
+          darkSalmon,
+          lavender,
+          lightCyan,
+          burlyWood,
+          plum,
+          gainsboro,
+          crimson,
+          paleVioletRed,
+          goldenrod,
+          orchid,
+          thistle,
+          lightGray,
+          tan,
+          chocolate,
+          peru,
+          indianRed,
+          mediumVioletRed,
+          silver,
+          darkKhaki,
+          rosyBrown,
+          mediumOrchid,
+          darkGoldenrod,
+          firebrick,
+          powderBlue,
+          lightSteelBlue,
+          paleTurquoise,
+          greenYellow,
+          lightBlue,
+          darkGray,
+          brown,
+          sienna,
+          darkOrchid,
+          paleGreen,
+          darkViolet,
+          mediumPurple,
+          lightGreen,
+          darkSeaGreen,
+          saddleBrown,
+          darkMagenta,
+          darkRed,
+          blueViolet,
+          lightSkyBlue,
+          skyBlue,
+          grey,
+          olive,
+          purple,
+          maroon,
+          aquamarine,
+          chartreuse,
+          lawnGreen,
+          mediumSlateBlue,
+          lightSlateGray,
+          slateGray,
+          oliveDrab,
+          slateBlue,
+          dimGray,
+          mediumAquamarine,
+          cornFlowerBlue,
+          cadetBlue,
+          darkOliveGreen,
+          indigo,
+          mediumTurquoise,
+          darkSlateBlue,
+          steelBlue,
+          royalBlue,
+          turquoise,
+          mediumSeaGreen,
+          limeGreen,
+          darkSlateGray,
+          seaGreen,
+          forestGreen,
+          lightSeaGreen,
+          dodgerBlue,
+          midNightBlue,
+          aqua,
+          cyan,
+          springGreen,
+          lime,
+          mediumSpringGreen,
+          darkTurquoise,
+          deepSkyBlue,
+          darkCyan,
+          teal,
+          green,
+          darkGreen,
+          blue,
+          mediumBlue,
+          darkBlue,
+          navy
+      };
+}
